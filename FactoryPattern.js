f478ffb5575ae43297c9021b5b8c83f571211b98
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-27 02:17:04 +0530

    Create FactoryPattern.js

---
+++ FactoryPattern.js
@@ -0,0 +1,54 @@
+it is a creational pattern . 
+factory is just an objct that create or manufacture different object..
+here factory is allowed to handle object createtion in centralized locaation
+it make code look clean
+/*
+creating new employ and inserting into database(employee array)
+*/
+function Developer(name)
+{
+  this.name = name
+  this.type = "Developer"
+}
+
+function Tester(name)
+{
+  this.name = name
+  this.type = "Tester"
+}
+
+function EmployeeFactory()
+{
+  this.create = (name, type) => {
+    switch(type)
+    {
+      case 1:
+        return new Developer(name)
+      case 2:
+        return new Tester(name)
+    }
+  }
+}
+
+function say()
+{
+  console.log("Hi, I am " + this.name + " and I am a " + this.type)
+}
+
+const employeeFactory = new EmployeeFactory()
+const employees = []
+
+employees.push(employeeFactory.create("Patrick", 1))
+employees.push(employeeFactory.create("John", 2))
+employees.push(employeeFactory.create("Jamie", 1))
+employees.push(employeeFactory.create("Taylor", 1))
+employees.push(employeeFactory.create("Tim", 2))
+
+employees.forEach( emp => {
+  say.call(emp)
+})
+
+
+ADV
+Modular expandability of the application	
+Good testability	
