6d94219c5100ac906198598daa0e19c4746098ae
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-06-27 13:41:02 +0530

    Update - Singleton Pattern.js

--- - Singleton Pattern.js
+++ - Singleton Pattern.js
@@ -2,8 +2,7 @@ Singleton pattern is a creational Pattern...it is use when we want to limit the 
 
 Ensure a class has only one instance, and provide a global point of access to it. So this one makes sure that only one instance exists. 
 If something attempts to make another, it simply returns a reference to the one that already exists. Easily understood.
-Example: database instances creation
-
+Example: database instances creation//only single instance can be created
 
 const Database = (function () {
   let instance;
